5edf89a57143ff575b7730163e314431dafad014
MarthaRCB <44388734+martharcb@users.noreply.github.com>
2019-08-30 06:16:42 -0700

    Using lab server locations

--- fluo4_analysis1.R
+++ fluo4_analysis1.R
@@ -1,8 +1,19 @@
-#set working directory and install packages
-setwd("~/UA/IL31 project/Ca imaging/ca_Ranalysis")
-library(tidyverse)
-install.packages("ggplot2")
-library(ggplot2)
+#This program will take logs of cell fluorescence values over time from a calcium
+#imaging experiment and use it to determine the number of responders and their response magnitudes
+#Updated by Martha Bhattacharya on 8/30/19
+
+#set working directory to server folder
+setwd("M:/Calcium Imaging Analysis")
+
+#load packages needed for the program to run. Current locations are Martha's 
+#hard drive but should be changed by the user.
+library("tidyverse", lib.loc="M:/Calcium Imaging Analysis/packagesR")
+library("tcltk", lib.loc="M:/Calcium Imaging Analysis/packagesR")
+library("ggplot2", lib.loc="M:/Calcium Imaging Analysis/packagesR")
+
+#make variables for data directory and output directory
+data.dir <- "M:/Calcium Imaging Analysis/rawdata"
+results.dir <- "M:/Calcium Imaging Analysis/outputR"
 
 #import and make data frame and time vectors
 well7 <- read.csv("well7_fluo4_Results.csv")
